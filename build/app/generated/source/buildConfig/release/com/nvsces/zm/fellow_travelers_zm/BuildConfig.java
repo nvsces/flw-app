61b5bdc9e01d1f8ee90040f36e10b79c9660fde3
nvsces <nvsces@yandex.ru>
2022-05-27 01:34:36 +0300

    files build

---
+++ build/app/generated/source/buildConfig/release/com/nvsces/zm/fellow_travelers_zm/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.nvsces.zm.fellow_travelers_zm;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.nvsces.zm.fellow_travelers_zm";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 5;
+  public static final String VERSION_NAME = "0.0.1";
+}
---
+++ build/flutter_login_vk/generated/source/buildConfig/release/ru/innim/flutter_login_vk/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ru.innim.flutter_login_vk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "ru.innim.flutter_login_vk";
+  public static final String BUILD_TYPE = "release";
+}
